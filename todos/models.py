75b86757523f9b43cf727f01e7fc59ebfb73feb9
unknown <vitor_hugo1934@hotmail.com>
2024-10-22 19:27:52 -0300

    Criação de migração e instalação do PEP

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100, null=False, blank=False)
+    created_at =models.DateField(auto_now_add=True,null=False,blank=False)
+    deadline = models.DateField(null=False, blank=False)
+    finished_at = models.DateField(null=True)
