1a59efb9872c1e115cb06a2bb1624ad390ab705f
unknown <vitor_hugo1934@hotmail.com>
2024-10-22 21:18:45 -0300

    Criação das paginas de tarefa

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Todo
+
 
 def home(request):
     return render(request, "home.html")
 
+class TodoListView(ListView):
+    model=Todo
+
+
+class TodoCrateView(CreateView):
+    model=Todo
+fields=["title","deadline"]
+success_url=reverse_lazy("todo_list")
+
+
+class TodoUpdateView(UpdateView):
+    model = Todo
+    fields = ['title', 'deadline']
+    success_url = reverse_lazy('todo_list')
+
+
+
+class TodoDeleteView(DeleteView):
+    model = Todo
+    success_url = reverse_lazy('todo_list')
